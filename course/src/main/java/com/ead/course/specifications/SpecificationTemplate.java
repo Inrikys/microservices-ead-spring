cb2a27407e810685aa51cbb7e77ad0e5289c8b39
Henrique Sousa <henrique661@gmail.com>
2025-01-09 19:23:16 -0300

    :sparkles: filtro e paginacao para lessons

--- course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -1,6 +1,7 @@
 package com.ead.course.specifications;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Root;
@@ -24,9 +25,12 @@ public class SpecificationTemplate {
     public interface CourseSpec extends Specification<CourseModel> {
     }
 
-
     @Spec(path = "title", spec = LikeIgnoreCase.class)
     public interface ModuleSpec extends Specification<ModuleModel> {
+    }
+
+    @Spec(path = "title", spec = LikeIgnoreCase.class)
+    public interface LessonSpec extends Specification<LessonModel> {
     }
 
     public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
@@ -35,7 +39,17 @@ public class SpecificationTemplate {
             Root<ModuleModel> module = root; // ENTITY A
             Root<CourseModel> course = query.from(CourseModel.class); // ENTITY B
             Expression<Collection<ModuleModel>> courseModules = course.get("modules"); // COLETANDO MODULOS DE DENTRO DO CURSO (ENTITY A em ENTITY B)
-            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, courseModules)); // CriteriaBuilder usando AND -> courseId igual ao recebido E membro de courseModules
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, courseModules)); // CriteriaBuilder usando AND -> courseId igual ao recebido E module membro de courseModules
+        };
+    }
+
+    public static Specification<LessonModel> lessonModuleId(final UUID moduleId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<LessonModel> lesson = root; // ENTITY A
+            Root<ModuleModel> module = query.from(ModuleModel.class); // ENTITY B
+            Expression<Collection<LessonModel>> moduleLessons = module.get("lessons"); // COLETANDO Lessons DE DENTRO DO Module (ENTITY A em ENTITY B)
+            return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(lesson, moduleLessons)); // CriteriaBuilder usando AND -> moduleId igual ao recebido E lesson membro de moduleLessons
         };
     }
 
